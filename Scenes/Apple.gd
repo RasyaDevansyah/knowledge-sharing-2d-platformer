118927caf8bb0a272b4e18fe21dd070db4850db7
Rasya <rasya.devan21@gmail.com>
2026-04-08 07:02:38 +0700

    added apple

---
+++ Scenes/Apple.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _on_body_entered(_body: Node2D) -> void:
+	animated_sprite_2d.play("collected")
+	
+	
+	pass # Replace with function body.
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite_2d.animation == "collected":
+		queue_free()
+	
+	pass # Replace with function body.
