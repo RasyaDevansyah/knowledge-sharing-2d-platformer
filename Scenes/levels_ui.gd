fe2e9ae97d38910f33eb4818c09bb09ddb87d54e
Rasya <rasya.devan21@gmail.com>
2026-05-12 23:03:54 +0700

    UI part done

---
+++ Scenes/levels_ui.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+func change_level(levelName : String):
+	
+	get_tree().change_scene_to_file(levelName)
+	
+	pass
---
+++ Scenes/quit_button.gd
@@ -0,0 +1,11 @@
+extends Button
+
+
+
+
+func _on_pressed() -> void:
+	
+	#logika untuk quit game
+	get_tree().quit()
+	
+	pass # Replace with function body.
